893eae760e44a798e3be2af82d114a3926bfa469
abugi <abugiboi@gmail.com>
2018-03-26 09:57:25 +0100

    commit

--- app.js
+++ app.js
@@ -55,6 +55,19 @@ app.use(expressValidator({
             msg: msg,
             value: value
         }
+    },
+
+    customValidators : {
+        isImage: function (value, filename) {
+            var extension = (path.extname(filename)).toLowerCase()
+            switch (extension){
+                case '.jpg': return '.jpg'
+                case '.jpeg': return '.jpeg'
+                case '.png': return '.png'
+                case '': return '.jpg'
+                default: return false
+            }
+        }
     }
 }))
 
--- routes/admin_products.js
+++ routes/admin_products.js
@@ -32,4 +32,83 @@ router.get('/add-product', function(req, res){
     })
 })
 
+//POST add product
+router.post('/add-product', function(req, res){
+
+    //To check that an image is uploaded
+    var imageFile = typeof req.files.image !== 'undefined' ? req.files.image.name : ""
+
+    req.checkBody('title', 'Title must have a value.').notEmpty()
+    req.checkBody('desc', 'Description must have a value.').notEmpty()
+    req.checkBody('price', 'Price must have a value.').isDecimal()
+
+    //custom validator for allowed image file extensions written in app.js line 60
+    req.checkBody('image', 'You must upload an image.').isImage(imageFile)
+
+    var title = req.body.title
+    var slug = title.replace(/\s+/g, '-')
+    var desc = req.body.desc
+    var price = req.body.price
+    var category = req.body.category
+
+    var errors = req.validationErrors()
+
+    if(errors){
+        Category.find(function (err, categories) {
+            res.render('admin/add_product', { title: title, desc: desc, categories: categories, price: price, errors: errors })
+        })
+    }else{
+        //Check product being added does not already exist
+        Product.findOne({slug: slug}, function(err,product){
+            if(product){
+                req.flash('danger', 'Product already exist, choose another')
+                Category.find(function (err, categories) {
+                    res.render('admin/add_product', { title: title, desc: desc, categories: categories, price: price })
+                })
+            }else{
+                var price2 = parseFloat(price).toFixed(2)
+
+                var product = new Product({
+                    title: title,
+                    desc: desc,
+                    price: price2,
+                    category: category,
+                    image: imageFile
+                })
+
+                product.save(function(err){
+                    if(err) return console.log(err)
+
+                    //create folders in the public directory
+                    mkdirp('public/product_images/' + product._id, function(err){
+                        if(err) return console.log(err)
+                    })
+
+                    mkdirp('public/product_images/' + product._id + '/gallery', function (err) {
+                        if (err) return console.log(err)
+                    })
+
+                    mkdirp('public/product_images/' + product._id + '/gallery/thumbs', function (err) {
+                        if (err) return console.log(err)
+                    })
+
+                    //check if image file is not an empty string
+                    if(imageFile != ""){
+                        var productImage = req.files.image
+                        var path = 'public/product_images/' + product._id + '/' + imageFile
+
+                        productImage.mv(path, function(err){
+                            return console.log(err)
+                        })
+                    }
+
+                    req.flash('success', 'Product Added')
+                    res.redirect('/admin/products')
+
+                })
+            }
+        })
+    }
+
+})
 module.exports = router
